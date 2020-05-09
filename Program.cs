4c1ad0d878e27d118d5b29bba9865c0edb956750
Kiran1805 <52439814+Kiran1805@users.noreply.github.com>
2020-05-09 22:38:25 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,44 +1,28 @@
 ﻿using System;
-using Microsoft.Azure.EventHubs;
-using Microsoft.Azure.EventHubs.Processor;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Azure.NotificationHubs;
 
-namespace AzureEventHubReceiver
+namespace PushNotificationSender
 {
     class Program
     {
-            private const string EventHubConnectionString = "Endpoint=sb://notifyhubkk.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=3p4bQKGnzZAsDZXB+5/WAuGrmfuyLluRIQEnjgK8Zwc=";
-            private const string EventHubName = "myfirsteventhub";
-            private const string StorageContainerName = "myfirstblob";
-            private const string StorageAccountName = "mystoragekk";
-            private const string StorageAccountKey = "DefaultEndpointsProtocol=https;AccountName=mystoragekk;AccountKey=8bGD702FRby/3sT561OnFhz7xV03yRxedOw/Lwf4NCNA4mOfLaD585FLivRYEvpbKQIl+goAw7gt7Js12OlbNA==;EndpointSuffix=core.windows.net";
-
-        //    private static readonly string StorageConnectionString = string.Format("DefaultEndpointsProtocol=https;AccountName={0};AccountKey={1}", StorageAccountName, StorageAccountKey);
-
-            public static void Main(string[] args)
-            {
-                MainAsync(args).GetAwaiter().GetResult();
-            }
-
-            private static async Task MainAsync(string[] args)
-            {
-                Console.WriteLine("Registering EventProcessor...");
-
-                var eventProcessorHost = new EventProcessorHost(
-                    EventHubName,
-                    PartitionReceiver.DefaultConsumerGroupName,
-                    EventHubConnectionString,
-                    StorageAccountKey,
-                    StorageContainerName);
-
-                // Registers the Event Processor Host and starts receiving messages
-                await eventProcessorHost.RegisterEventProcessorAsync<SimpleEventProcessor>();
-
-                Console.WriteLine("Receiving. Press ENTER to stop worker.");
-                Console.ReadLine();
-
-                // Disposes of the Event Processor Host
-                await eventProcessorHost.UnregisterEventProcessorAsync();
-            }
+        static void Main(string[] args)
+        {
+            SendNotificationAsync();
+            Console.ReadKey();
         }
+        public static async void SendNotificationAsync()
+        {
+            NotificationHubClient hub = NotificationHubClient
+                .CreateClientFromConnectionString(
+                "Endpoint=sb://notofiyhubkk.servicebus.windows.net/;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=2yZYnkGXywUsFeRhlWXcxIAXIIWY02FAX0CnZkTyfOk=",
+                "myfirstnotifyhub");
+            var toast = @"<toast><visual><binding template=""ToastText01""><text id=""1"">Grab 10% Cashback on your Dish TV Recharges</text></binding></visual></toast>";
+            await hub.SendWindowsNativeNotificationAsync(toast);
+        }
+
+    }
 }
